87d98da09bf2c0582016336fed2437cec5a8962b
yamakoshi <t.yama.pijotts@gmail.com>
2020-03-25 16:08:44 +0900

    bug fixed

--- text_cnn.py
+++ text_cnn.py
@@ -66,7 +66,7 @@ class TextCNN(object):
             W = tf.get_variable(
                 "W",
                 shape=[num_filters_total, num_classes],
-                initializer=tf.initializer.layers.GlorotUniform())
+                initializer=tf.initializers.glorot_uniform())
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
